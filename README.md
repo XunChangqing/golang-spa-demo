aa46b12bd3c5a2d777c9fae914a9cce496becc41
xun changqing <xunchangqing@qq.com>
2020-09-05 15:17:48 +0800

    WORK: 完善注释和readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # golang-spa-demo
+这个简单的程序演示了一个单页应用的主体结构和前后端交互过程。
+后端使用golang编写，只依赖于标准库的http和json库，提供一个非常简单的/api/add接口，将输入的两个整数相加并返回结果。
+golang后端同时提供对html文件的静态访问服务。
+后端只有一个主程序文件cmd/main.go
+
+前端使用html+javascript编写，两个输入框，用户点击计算以后，javascript向后端发出请求，获得结果以后更细界面元素来呈现。
+前端代码都在web/index.html中
+
+# 使用方法
+
+cd cmd
+go build main.go
+./main
+
+打开浏览器,输入http://localhost:8080/web/index.html
+在页面内输入两个数字，然后点击Calculate，结果会在下面显示
+
+# 作业
+使用C#或是C++（MFC）编写一个客户端程序，通过调用golang的/api/add接口来实现一个加法程序，讨论并说明CS和BS结构的异同。
--- cmd/main.go
+++ cmd/main.go
@@ -16,17 +16,26 @@ type AddReply struct {
 }
 
 func main() {
+	// 处理发往/api/add的http请求
 	http.HandleFunc("/api/add", func(w http.ResponseWriter, r *http.Request) {
 		var add_req AddReq
+		// 将请求的body作为JSON字符串解码，并存入AddReq结构体内
 		json.NewDecoder(r.Body).Decode(&add_req)
+		// 打印请求数据
 		log.Println("req: ", add_req.OperandA, add_req.OperandB)
 		var add_reply AddReply
+		// 进行加法计算，并保存结果到结构体内
 		add_reply.Result = add_req.OperandA + add_req.OperandB
+		// 将结果结构体进行JSON编码，并写入响应
 		json.NewEncoder(w).Encode(add_reply)
 	})
 
+	// 使用web目录下的文件来响应对/web/路径的http请求，一般用作静态文件服务，例如html、javascript、css等
 	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("../web/"))))
 
+	// 启动http服务
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// 可以使用如下curl命令来访问加法API
 	// curl -s -XPOST -d'{"OperandA":10,"OperandB":20}' http://localhost:8080/api/add
 }
